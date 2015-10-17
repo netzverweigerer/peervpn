02bdca46f3b188caa9c12a4e714d932f2e7845dc
armin <armin@voodoo.vpn.blue>
2015-10-17 12:54:47 +0200

    strip development infriendly boilerplates

--- globals.c
+++ globals.c
@@ -1,44 +1,22 @@
-/***************************************************************************
- *   Copyright (C) 2015 by Tobias Volk                                     *
- *   mail@tobiasvolk.de                                                    *
- *                                                                         *
- *   This program is free software: you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation, either version 3 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
- ***************************************************************************/
 
 
-// version information
 #define PEERVPN_VERSION_MAJOR 0
 #define PEERVPN_VERSION_MINOR 43
 
 
-// compile time options & timing parameters
 #define INITPEER_STORAGE 1024
 
 
-// config parser options
 #define CONFPARSER_LINEBUF_SIZE 4096
 #define CONFPARSER_NAMEBUF_SIZE 512
 
 
-// iogrps
 #define IOGRP_DEFAULT 0
 #define IOGRP_SOCKET 1
 #define IOGRP_TAP 2
 #define IOGRP_CONSOLE 3
 
 
-// global variables
 struct s_io_state iostate;
 P2PSEC_CTX *g_p2psec = NULL;
 int g_mainloop;
